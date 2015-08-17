27afa3857cf00c7e74c6e01d56599f030248387b
David Royer <droyer01@gmail.com>
2015-08-17 15:21:43 -0400

    Deleted navigation css for starting point for viewers

--- navigation.js
+++ navigation.js
@@ -2,18 +2,11 @@
 
 // Cache Dom
 
-var navTrigger = $('.nav__trigger');
-var nav = $('.nav');
+
 
 
 // Function
 
-function toggleMenu(event) {
 
-	event.preventDefault();
-	nav.toggleClass('nav--open');
-}
 
 // Bind Function
-
-navTrigger.on('click', toggleMenu);
